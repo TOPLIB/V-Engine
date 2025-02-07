b8d5d7b52fc99de25c6d44ad1c92d637597454f5
TOPLIB <hourgamerfor@gmail.com>
2025-02-07 21:12:59 +0300

    Creating the foundation of the future engine

--- src/com/engine/vengine/Storage/DataStorage.java
+++
@@ -1,8 +0,0 @@
-package com.engine.vengine.Storage;
-
-public interface DataStorage<V> {
-    void set(String key, V value);
-    V get(String key);
-    boolean has(String key);
-    void remove(String key);
-}
